eab79ffbe10df34edd5668d73bf2f7dcb03a6b01
javier <jagalu@campusaula.com>
2025-01-27 10:52:51 +0100

    Boton Jugar Funcional

--- JS.js
+++ JS.js
@@ -2,6 +2,7 @@
 
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
+var jugar = document.getElementById("jugar");
 
 // Variables juego
 
@@ -15,13 +16,28 @@ var puntuacionMuerte = 0;
 // Código
 
 function Muerte(){
-    if(puntuacionMuerte => 5){
+    if(puntuacionMuerte >= 5){
         puntuacion = 0;
-        death_counter + 1;
+        death_counter++;
     }
 }
 
+jugar.addEventListener('click', dibujaCirculo);
 
+function dibujaCirculo()
+{
+    ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+
+    var x = Math.random() * canvas.clientWidth;
+    var y = Math.random() * canvas.clientHeight;
+
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "black";
+
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, 2* Math.PI);
+    ctx.stroke();
+}
 
 
 
